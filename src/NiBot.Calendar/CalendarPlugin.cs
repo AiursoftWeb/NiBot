a9eb7c1227a5cc532e4f4034d4820651d123203b
Anduin Xue <anduin@aiursoft.com>
2023-07-20 16:55:26 +0000

    Add unresolved files.

---
+++ src/NiBot.Calendar/CalendarPlugin.cs
@@ -0,0 +1,15 @@
+﻿using Aiursoft.CommandFramework.Abstracts;
+using Aiursoft.CommandFramework.Framework;
+
+namespace Aiursoft.NiBot.Calendar;
+
+public class CalendarPlugin : IPlugin
+{
+    public CommandHandler[] Install()
+    {
+        return new CommandHandler[]
+        {
+            new CalendarHandler(),
+        };
+    }
+}
