11d8637751fa72a169b577a5525f9f80416c44bc
EdgeNeko <edgeneko@aiursoft.com>
2024-06-25 23:34:49 +0800

    Fix Lint

--- src/NiBot.Dedup/Util/Dsu.cs
+++ src/NiBot.Dedup/Util/Dsu.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-
-namespace NiBot.Dedup.Util;
+﻿namespace NiBot.Dedup.Util;
 
 public class Dsu
 {
--- src/NiBot.Dedup/Util/VPTree.cs
+++ src/NiBot.Dedup/Util/VPTree.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 namespace NiBot.Dedup.Util;
 
